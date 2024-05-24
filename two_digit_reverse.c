6e78f1d52ca8e79c60ea51fe16e9c13c88653fe6
Cameron <cameronplauche@gmail.com>
2024-05-24 10:56:26 -0500

    three digits

--- two_digit_reverse.c
+++ two_digit_reverse.c
@@ -10,9 +10,9 @@ The reversal is: 82
 #include <stdio.h>
 
 int main(void) {
-    int x, y;
-    printf("Enter two digit number: ");
-    scanf("%1d%1d", &y, &x);
-    printf("The reversal is %d%d\n", y, x);
+    int x, y, z;
+    printf("Enter three digit number: ");
+    scanf("%1d%1d%d", &x, &y, &z);
+    printf("The reversal is %d%d%d\n", z, y, x);
     return 0;
 }
